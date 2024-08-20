0ea979a1d4cfd9e4b1973fd3c885dd5f0261550a
unknown <senthamil1912@gmail.com>
2024-08-20 14:47:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+alert('test');
+
+setTimeout(()=>document.getElementsByClassName('name')[0].innerHTML='change name',3000)
+
+setTimeout(()=>document.getElementsByClassName('name')[1].innerHTML='',7000)
+
+alert(6+6)
+
+console.log('output')
